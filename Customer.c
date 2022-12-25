10f9e5665ea36f46cf14214f0462aac5efc876bd
Idan <idunsky855@gmail.com>
2022-12-25 17:57:34 +0200

    MODIFIED CUSTOMER.C

--- Customer.c
+++ Customer.c
@@ -7,28 +7,32 @@
 
 int initCustomer(Customer* pCustomer) {
 
-	pCustomer->name = createDynamicStr("Please enter customers name : ");
+	pCustomer->name = createDynamicStr("Please enter customers name: ");
 	if (!pCustomer->name)
 		return 0;
-
-	pCustomer->pCart = (ShoppingCart*)malloc(sizeof(ShoppingCart));
-	if (!pCustomer->pCart) {
-		free(pCustomer->name);
-		return 0;
-	}
-
-	if (!initShoppingCart(pCustomer->pCart)) {
-		freeCustomer(pCustomer);
-		return 0;
-	}
-
+	pCustomer->pCart = NULL; //Empty cart
+	
 	return 1;
 }
+
 void freeCustomer(Customer* pCustomer) {
-
+	free(pCustomer->name);
+	if (!pCustomer->pCart) {
+		free(pCustomer->pCart);
+	}
+	free(pCustomer);
 }
-void printCustomer(const Customer* pCustomer) {
 
+
+
+void printCustomer(const Customer* pCustomer) {
+	printf("Customer's name: %s", pCustomer->name);
+	printf("\t\t");
+	if (!pCustomer->pCart) {
+		printf("Shopping cart is empty!\n\n");
+		return;
+	}
+	printf("Doing shopping now!!!");
 }
 
 
@@ -37,6 +41,7 @@ char* createDynamicStr(const char* msg) {
 	char temp[MAX_STR_LEN];
 	printf(msg);
 
+	fgets(temp, MAX_STR_LEN, stdin);
 	str = (char*)malloc((strlen(temp) + 1) * sizeof(char));
 	if (!str)
 		return NULL;
